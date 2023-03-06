cd5d197ab705a26fdced642d0c6403de8627935a
Alexey Garkaviy <wevioz@mail.ru>
2023-03-06 20:17:41 +0300

    task-1

---
+++ Day-1/task-1/task-1/task-1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace task1
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Int64 x = Convert.ToInt64(Console.ReadLine());
+
+            if (x >= 4 && x <= 6)
+            {
+                Console.WriteLine(x);
+            }
+            else if (x > 6)
+            {
+                Console.WriteLine(3 * x + 4 * Math.Pow(x, 2));
+            }
+        }
+    }
+}
