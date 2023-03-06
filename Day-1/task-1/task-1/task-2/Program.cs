7e44eedb482f5f86707ef18984562b1d5313bea0
Alexey Garkaviy <wevioz@mail.ru>
2023-03-06 22:23:46 +0300

    task-2

---
+++ Day-1/task-1/task-1/task-2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace task2
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            string x = Console.ReadLine();
+
+            Console.WriteLine(
+                x[0] == x[1] &&
+                x[0] == x[2] &&
+                x[1] == x[0] &&
+                x[1] == x[2] &&
+                x[2] == x[0] &&
+                x[2] == x[1]
+            );
+        }
+    }
+}
