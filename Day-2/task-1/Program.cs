5ae49cd799b4208c390bdff4704b6118d0733e69
Wevioz <wevioz@mail.ru>
2023-03-13 08:58:46 +0300

    task-1

---
+++ Day-2/task-1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace task1
+{
+    class A
+    {
+        public double a, b;
+
+        public A(double a, double b)
+        {
+            this.a = a;
+            this.b = b;
+        }
+
+        public double calculate()
+        {
+            return 1 / (1 + (a + b) / 2);
+        }
+
+        public double insertInFormula()
+        {
+            return Math.Pow((a - b), 2);
+        }
+    }
+
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            double a = Convert.ToDouble(Console.ReadLine());
+            double b = Convert.ToDouble(Console.ReadLine());
+
+            A aClass = new A(a, b);
+
+            Console.WriteLine(aClass.calculate());
+
+            Console.WriteLine(aClass.insertInFormula());
+        }
+    }
+}
