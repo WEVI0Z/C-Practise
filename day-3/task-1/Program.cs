7b050a942e55d1419d0baf10d7c43c2cae279054
Wevioz <wevioz@mail.ru>
2023-03-13 09:49:49 +0300

    task-1

---
+++ day-3/task-1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace task1
+{
+    class MainClass
+    {
+        class TwoDimArr
+        {
+            public int[][] array;
+            private int arrayLength;
+            private int secondDimensionLength;
+
+            public TwoDimArr(int[][] array)
+            {
+                this.array = array;
+                arrayLength = array.Length;
+                secondDimensionLength = array[0].Length;
+            }
+
+            public int[][] multWithArr(int[][] additionalArr)
+            {
+                if (
+                    additionalArr.Length == this.arrayLength &&
+                    additionalArr[0].Length == this.secondDimensionLength
+                    )
+                {
+                    for(int i = 0; i < this.arrayLength; i++)
+                    {
+                        for(int j = 0; j < this.secondDimensionLength; j++)
+                        {
+                            this.array[i][j] *= additionalArr[i][j];
+                        }
+                    }
+                }
+
+                return this.array;
+            }
+        }
+        public static void Main(string[] args)
+        {
+
+        }
+    }
+}
