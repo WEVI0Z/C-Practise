cc243eb9b9a69cd33f43f1b86d05809ef67b6847
Wevioz <wevioz@mail.ru>
2023-03-18 13:00:40 +0300

    first task

--- day-4/task-1/Program.cs
+++ day-4/task-1/Program.cs
@@ -6,7 +6,24 @@ namespace task1
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            double x = 1;
+
+            try
+            {
+                double firstY = 1 - 4 / (4 * x + 8);
+                double secondY = Math.Pow(Math.Tan(x), 2) + 1 / (x - 1);
+
+                Console.WriteLine(firstY);
+                Console.WriteLine(secondY);
+            }
+            catch(DivideByZeroException)
+            {
+                Console.log("Devide by zero exception");
+            }
+            catch (FormatException)
+            {
+                Console.log("Format exception");
+            }
         }
     }
 }
