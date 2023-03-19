1087478cec732c26f8c18b3f138195151b1ccd91
Wevioz <wevioz@mail.ru>
2023-03-19 12:05:42 +0300

    task-5

---
+++ day-5/task-5/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace task5
+{
+    class MainClass
+    {
+        static int Factorial(int n)
+        {
+            if (n == 0 || n == 1)
+            {
+                return 1;
+            }
+            else
+            {
+                return n * Factorial(n - 1);
+            }
+        }
+
+        static double F(int n)
+        {
+            if (n == 0 || n == 1)
+            {
+                return 1;
+            }
+            else
+            {
+                int factorialNMinus1 = Factorial(n - 1);
+                int factorialN = Factorial(n);
+                return (double)factorialNMinus1 / factorialN;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Write("Введите значение n: ");
+            int n = int.Parse(Console.ReadLine());
+
+            double result = F(n);
+            Console.WriteLine($"f({n}) = {result}");
+        }
+    }
+}
