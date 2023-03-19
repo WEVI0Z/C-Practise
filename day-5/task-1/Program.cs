016b8078174c0667823426dde1390204a1d8dbbb
Wevioz <wevioz@mail.ru>
2023-03-19 11:40:36 +0300

    task-1

---
+++ day-5/task-1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace task1
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int[] arr = { 4, 3, 5, 9, 6, 12 };
+            int sum = 0;
+
+            for(int i = 0; i < arr.Length; i++)
+            {
+                sum += arr[i] % 3 == 0 ? arr[i] : 0;
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
