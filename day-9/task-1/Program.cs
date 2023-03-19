9bf4240066cd68ca76c75da7abc7623e2edc255d
Wevioz <wevioz@mail.ru>
2023-03-19 17:16:50 +0300

    task-2

---
+++ day-9/task-1/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+
+namespace task1
+{
+    interface Ix
+    {
+        void IxF0(int parameter);
+        void IxF1();
+    }
+
+    interface Iy
+    {
+        void F0(int parameter);
+        void F1();
+    }
+
+    interface Iz
+    {
+        void F0(int parameter);
+        void F1();
+    }
+
+    class TestClass : Ix, Iy, Iz
+    {
+        public int w;
+
+        // Неявная реализация методов интерфейсов Ix и Iy
+        public void IxF0(int parameter)
+        {
+            Console.WriteLine(parameter * 10);
+        }
+
+        public void IxF1()
+        {
+            Console.WriteLine(10 * w);
+        }
+
+        public void F0(int parameter)
+        {
+            Console.WriteLine(parameter - 10);
+        }
+
+        public void F1()
+        {
+            Console.WriteLine(w - 10);
+        }
+
+        // Явная реализация методов интерфейса Iz
+        void Iz.F0(int parameter)
+        {
+            Console.WriteLine(parameter / 10);
+        }
+
+        void Iz.F1()
+        {
+            Console.WriteLine(w / 10);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TestClass test = new TestClass();
+            test.w = 20;
+
+            // Вызов функций с явным приведением к типу интерфейса
+            Ix ix = test;
+            ix.IxF0(30);
+            ix.IxF1();
+
+            Iy iy = test;
+            iy.F0(50);
+            iy.F1();
+
+            Iz iz = (Iz)test;
+            iz.F0(40);
+            iz.F1();
+
+            // Вызов метода для объекта посредством интерфейсной ссылки
+            CallIxF0(test);
+            CallF0(test);
+            CallIzF0(test);
+        }
+
+        static void CallIxF0(Ix ix)
+        {
+            ix.IxF0(10);
+        }
+
+        static void CallF0(Iy iy)
+        {
+            iy.F0(5);
+        }
+
+        static void CallIzF0(Iz iz)
+        {
+            iz.F0(7);
+        }
+    }
+
+}
---
+++ day-9/task-2/Program.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace task2
+{
+    interface IRecord
+    {
+        string LastName { get; set; }
+        string FirstName { get; set; }
+        DateTime BirthDate { get; set; }
+        string PhoneNumber { get; set; }
+    }
+
+    interface INotebook
+    {
+        void AddRecord(IRecord record);
+        void RemoveRecord(int index);
+        void SortByLastName();
+        List<IRecord> FindRecords(string query);
+        IRecord GetRecord(int index);
+        int Count { get; }
+    }
+
+    class Record : IRecord
+    {
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+
+    class Notebook : INotebook
+    {
+        public List<IRecord> records = new List<IRecord>();
+
+        public void AddRecord(IRecord record)
+        {
+            records.Add(record);
+        }
+
+        public void RemoveRecord(int index)
+        {
+            records.RemoveAt(index);
+        }
+
+        public void SortByLastName()
+        {
+            records.Sort((a, b) => a.LastName.CompareTo(b.LastName));
+        }
+
+        public List<IRecord> FindRecords(string query)
+        {
+            return records.FindAll(record =>
+                record.LastName.Contains(query) ||
+                record.FirstName.Contains(query) ||
+                record.PhoneNumber.Contains(query));
+        }
+
+        public IRecord GetRecord(int index)
+        {
+            return records[index];
+        }
+
+        public int Count => records.Count;
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var notebook = new Notebook();
+
+            var record2 = new Record { LastName = "Petin", FirstName = "Petr", BirthDate = new DateTime(1980, 2, 2), PhoneNumber = "222-22-22" };
+            notebook.AddRecord(record2);
+
+            var record3 = new Record { LastName = "Sidorik", FirstName = "Sidor", BirthDate = new DateTime(1970, 3, 3), PhoneNumber = "333-33-33" };
+            notebook.AddRecord(record3);
+
+            var record1 = new Record { LastName = "Ivanov", FirstName = "Ivan", BirthDate = new DateTime(1990, 1, 1), PhoneNumber = "111-11-11" };
+            notebook.AddRecord(record1);
+
+            Console.WriteLine($"Count: {notebook.Count}");
+
+            Console.WriteLine("All records:");
+            for (int i = 0; i < notebook.Count; i++)
+            {
+                var record = notebook.GetRecord(i);
+                Console.WriteLine($"{i + 1}. {record.LastName} {record.FirstName}, {record.BirthDate.ToString("yyyy-MM-dd")}, {record.PhoneNumber}");
+            }
+
+            Console.WriteLine("Find by last name:");
+            var foundRecords = notebook.FindRecords("ov");
+            for (int i = 0; i < foundRecords.Count; i++)
+            {
+                var record = foundRecords[i];
+                Console.WriteLine($"{i + 1}. {record.LastName} {record.FirstName}, {record.BirthDate.ToString("yyyy-MM-dd")}, {record.PhoneNumber}");
+            }
+
+            notebook.RemoveRecord(1);
+
+            Console.WriteLine("Sort:");
+            notebook.SortByLastName();
+            for (int i = 0; i < notebook.Count; i++)
+            {
+                var record = notebook.GetRecord(i);
+                Console.WriteLine($"{i + 1}. {record.LastName} {record.FirstName}, {record.BirthDate.ToString("yyyy-MM-dd")}, {record.PhoneNumber}");
+            }
+        }
+    }
+}
