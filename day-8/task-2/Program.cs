b4aa3cd4bbefdcd59ebd442c3bc01becd19bab46
Wevioz <wevioz@mail.ru>
2023-03-19 16:44:08 +0300

    task-2

---
+++ day-8/task-2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace task2
+{
+    class MainClass
+    {
+        enum Position
+        {
+            Manager = 160,
+            Engineer = 176,
+            Developer = 192,
+            Designer = 144,
+            Analyst = 168
+        }
+
+        class Accauntant
+        {
+            public bool AskForBonus(Position worker, int hours)
+            {
+                if (hours > (int)worker)
+                {
+                    return true;
+                }
+                return false;
+            }
+        }
+
+        public static void Main(string[] args)
+        {
+            Accauntant accauntant = new Accauntant();
+
+            Console.WriteLine(accauntant.AskForBonus(Position.Analyst, 10));
+        }
+    }
+}
