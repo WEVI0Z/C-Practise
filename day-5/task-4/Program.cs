428c2f24faf7a8401c15d89bf4935aab7602393f
Wevioz <wevioz@mail.ru>
2023-03-19 12:03:24 +0300

    task-4

---
+++ day-5/task-4/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace task4
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int[,] array = new int[3, 4] {
+                { 1, 2, 3, 4 },
+                { 5, 6, 7, 8 },
+                { 9, 10, 11, 12 }
+            };
+
+            int columnNumber = 2;
+            int divisor = 3;
+
+            int sum = 0;
+
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                sum += array[i, columnNumber];
+            }
+
+            if (sum % divisor == 0)
+            {
+                Console.WriteLine($"Сумма элементов столбца {columnNumber} кратна {divisor}");
+            }
+            else
+            {
+                Console.WriteLine($"Сумма элементов столбца {columnNumber} не кратна {divisor}");
+            }
+
+        }
+    }
+}
