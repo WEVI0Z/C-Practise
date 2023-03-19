00861df73b2e654bebba7cabc329514e6eb40639
Wevioz <wevioz@mail.ru>
2023-03-19 15:02:50 +0300

    task-3

---
+++ day-7/task-2/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace task2
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            string text = "Заданный текст со словом около.";
+
+            Console.WriteLine(text.Replace("о", "оOk"));
+        }
+    }
+}
---
+++ day-7/task-3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace task3
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            string text = "Текст с числами 123 и 456.";
+            double product = 1;
+
+            string pattern = @"\d+";
+            Regex regex = new Regex(pattern);
+
+            foreach (Match match in regex.Matches(text))
+            {
+                double number = double.Parse(match.Value);
+                product *= number;
+            }
+
+            Console.WriteLine("Произведение чисел в тексте: " + product);
+        }
+    }
+}
